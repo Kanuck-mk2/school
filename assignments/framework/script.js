1a7d4969275aeaf01de297c04fa2d00f2e2d2df8
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2024-09-12 20:06:45 -0600

    update

---
+++ assignments/framework/script.js
@@ -0,0 +1,45 @@
+const pancake = [
+  {
+    id: 1,
+    name: 'string for now',
+    grade: 1,
+    description: 'string for now',
+    attendance: true,
+  },
+
+  {
+    id: 1,
+    name: 'string for now',
+    grade: 1,
+    description: 'string for now',
+    attendance: true,
+  },
+
+  {
+    id: 1,
+    name: 'string for now',
+    grade: 1,
+    description: 'string for now',
+    attendance: true,
+  },
+
+  {
+    id: 1,
+    name: 'string for now',
+    grade: 1,
+    description: 'string for now',
+    attendance: true,
+  },
+
+  {
+    id: 1,
+    name: 'string for now',
+    grade: 1,
+    description: 'string for now',
+    attendance: true,
+  },
+];
+
+const lesson = (pancake, time) => {};
+
+console.log('Hello Class');
