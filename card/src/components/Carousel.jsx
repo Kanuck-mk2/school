5987545faae71a2bdf5dcced81c4c5264eb5efa1
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2024-09-16 23:32:43 -0600

    updated responsiveness in card component

--- card/src/components/Carousel.jsx
+++ card/src/components/Carousel.jsx
@@ -88,7 +88,7 @@ const Carousel = () => {
       style={{ backgroundImage: `url(${bgD2})` }} // Use backgroundImage here
     >
       <motion.div
-        className="sm:grid grid-flow-col grid-cols-2 space-x-6 overflow-hidden p-11"
+        className="lg:flex flex-row sm:grid grid-flow-col grid-cols-2 space-x-6 overflow-hidden p-11"
         animate={{ x: -currentIndex * 350 }} // Adjust slide distance per card width
         transition={{ type: 'spring', stiffness: 200, damping: 60 }}
       >
