7a06df8ac461b248fa56b623c082ff3ffb047be2
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2024-09-18 21:38:16 -0600

    update

--- card/src/components/Section.tsx
+++ card/src/components/Section.tsx
@@ -1,7 +1,9 @@
+import { motionValue, useAnimate, useMotionValue, useTransform } from 'framer-motion';
 import React, { useState } from 'react';
 
 const Section = () => {
   const [time, setTime] = useState(0);
+  const [moveSet, setMoveSet] = useAnimate(0);
   const medis = [
     {
       id: 1,
@@ -24,6 +26,13 @@ const Section = () => {
       power: '',
     },
   ];
+
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
+  const attack = () => {
+    setMoveSet((MouseEvent) => motionValue);
+  };
   return (
     <div className="w-full h-screen relative border-1 border-sky-500">
       <h1 className="text-sky-400 text-5xl">Title 1</h1>
