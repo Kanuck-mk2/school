ea796fe179e71933b9904d61b1d6a34b81c6a806
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2024-09-18 21:19:51 -0600

    added a section.tsx component

--- card/src/components/Section.tsx
+++ card/src/components/Section.tsx
@@ -5,29 +5,37 @@ const Section = () => {
   const medis = [
     {
       id: 1,
-      name: '',
+      name: 'medi1',
       power: '',
     },
     {
-      id: 1,
-      name: '',
+      id: 2,
+      name: 'medi2',
       power: '',
     },
     {
-      id: 1,
-      name: '',
+      id: 3,
+      name: 'medi3',
       power: '',
     },
     {
-      id: 1,
-      name: '',
+      id: 4,
+      name: 'medi4',
       power: '',
     },
   ];
   return (
     <div className="w-full h-screen relative border-1 border-sky-500">
-      <h1 className="text-sky-400 text-5xl"></h1>
-      Section
+      <h1 className="text-sky-400 text-5xl">Title 1</h1>
+      <div className="">
+        {medis.map((medi, index) => (
+          <div key={medi.id} className="p-4 border-b">
+            <div>{medi.id}</div>
+            <div>{medi.name}</div>
+            <div>{medi.power}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
