5e2d9f774f503aa886091950e77d0b22a1fdff70
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2024-09-21 23:06:47 -0600

    more coding around

--- for-fun/src/pages/Hero.tsx
+++ for-fun/src/pages/Hero.tsx
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+
 const Hero = () => {
+  interface={
+    mouseX: 'number'
+  },
+  const [imageProfile, setImageProfile] = (useState = 0);
+
+  const mouseX = mouseX.onmousemove(0);
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center"><section className='border-1 border-red-600'>
+       <h1 className="text-5xl text-blue-700">Hello World</h1>
+       <p className="text-teal-200">my anme is chad</p>
+    </section>
     
-      <h1 className="text-5xl text-blue-700">Hello World</h1>
     </div>
   );
 };
