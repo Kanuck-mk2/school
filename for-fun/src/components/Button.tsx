ad5aa031665a756cd41e55e27f6c1479833b37e2
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2024-10-13 21:38:36 -0600

    sure, its an update

--- for-fun/src/components/Button.tsx
+++ for-fun/src/components/Button.tsx
@@ -12,6 +12,7 @@ const Button = () => {
     <div className="flex justify-center items-center h-screen">
       {/* Outer button */}
       <button
+        type="button"
         className={`${
           classSet === 0 ? 'bg-blue-500' : 'bg-violet-500'
         } text-white font-bold py-2 px-4 rounded transition-all duration-300`}
