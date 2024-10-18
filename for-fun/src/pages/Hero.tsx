f1cf9446e929e651dc453f11f369115879a0ea68
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2024-10-18 01:49:08 -0600

    some coding for once

--- for-fun/src/pages/Hero.tsx
+++ for-fun/src/pages/Hero.tsx
@@ -1,24 +1,18 @@
+import jupiterImage from '../assets/jupiter.jpg';
+
+
+
 const Hero: React.FC = () => {
   return (
-    <div className="flex  flex-row items-center justify-center h-screen min-w-max absolute top-0 border-1 border-green-600">
-      <section className=" flex flex-row border-1 border-red-600 p-4">
-        <h1 className="text-5xl text-blue-700">Hello World</h1>
-        <p className="text-teal-200">This is my Solar system website</p>
-
-        <section className="flex flex-row items-center justify-center border-1 border-sky-300 p-4 ml-4">
-          <div className="space-y-4">
-            <h2 className="text-5xl text-sky-300">
-             This is section 1
-            </h2>
-            <p className="text-3xl text-rose-400">The hero section</p>
-            <div className="w-20 h-20 bg-gray-300">
-              <p>More bullshit to code shit</p>
-              <img alt="stuff"></img>
-            </div>
-            <div className="w-20 h-20 bg-gray-300"></div>
-          </div>
-        </section>
-      </section>
+    <div className="w-full h-screen relative">
+      <img
+        className="w-full h-screen object-cover object-center"
+        src={jupiterImage}
+        alt="Neptune"
+      />
+      <div className="w-full h-screen absolute top-0 left-0 flex justify-center items-center">
+        <h1 className='flex top-0 text-5xl text-sky-400'>Hello World</h1>
+      </div>
     </div>
   );
 };
