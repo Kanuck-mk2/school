be11215305b0aa223528e14afc49d99de7f6c47a
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2024-10-19 13:46:50 -0600

    small update

--- for-fun/src/pages/Hero.tsx
+++ for-fun/src/pages/Hero.tsx
@@ -1,17 +1,21 @@
-import voyagerImage from '../assets/voyager-2.jpg';
-
-
+import voyagerImage from '/assets/voyager-2.jpg';
 
 const Hero: React.FC = () => {
   return (
-    <div id='home' className="w-full h-screen relative ">
+    <div id="home" className="w-full h-screen relative ">
       <img
         className="w-full h-screen object-cover object-center"
         src={voyagerImage}
         alt="Voyager"
       />
-      <div className="w-full h-screen absolute top-0 left-0 flex justify-center items-center">
-        <h1 className='flex top-0 text-5xl text-sky-400 font-orbitron text-glow'>Voyager 2</h1>
+      <div className=" border w-full h-screen absolute top-0 left-0 flex flex-col justify-center items-center">
+        <h1 className=" flex top-0 text-sky-400 font-orbitron text-6xl ">
+          Voyager 2
+        </h1>
+
+        <h2 className="text-sky-400 font-orbitron text-4xl p-3">
+          The spacecraft and mission
+        </h2>
       </div>
     </div>
   );
